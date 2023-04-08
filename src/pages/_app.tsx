bf0ef69306cf5e96afbd64cd499baa18964f18de
binhphancreator <tienkhach19@gmail.com>
2023-04-08 12:07:52 +0700

    tailwind postcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "~/store";
+import "~/styles/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
